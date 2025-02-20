a015dae23ccb95876d8226b068c00413e4b66a05
krik.chen <1449262967@qq.com>
2025-02-20 22:44:36 +0800

    该分支调整标签顺序后,重新验证了baseline,以及所提模块,发现AFPN效果一般,下一步寻求代替模块git add .

--- train.py
+++ train.py
@@ -3,13 +3,17 @@ from ultralytics import YOLO
 
 if __name__ == '__main__':
     # Load a model
-    model = YOLO('yolov8s-C2fiEMA.yaml')  # build a new model from YAML
+    # model = YOLO('yolov8s.yaml')  # build a new model from YAML
+    # model = YOLO('yolov8s-p2.yaml')  # build a new model from YAML
+    # model = YOLO('yolov8s-C2fiEMA.yaml')  # build a new model from YAML
+    # model = YOLO('yolov8s-C2fiEMA-inMPDIOU.yaml')  # build a new model from YAML
+    model = YOLO('yolov8s-C2fiEMA-inMPDIOU-AFPN.yaml')  # build a new model from YAML
     model.train(data=r"ultralytics/cfg/datasets/tomato.yaml",
                 cache = False,
                 epochs=100, 
                 imgsz=640,
                 batch=32,
                 optimizer='Adam',
-                workers = 8,
+                workers = 12,
                 amp = True
                 )
